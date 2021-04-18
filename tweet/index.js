fcd03793232510d8f95c7c86bbe74587e1e8afdf
maniteja <pratamaniteja66@gmail.com>
2021-04-18 20:05:00 +0530

    minor bug fixes

--- tweet/index.js
+++ tweet/index.js
@@ -1,6 +1,6 @@
 const Twit = require("twit");
 
-// require("dotenv").config("../.env");
+require("dotenv").config("../.env");
 
 const twit = new Twit({
   consumer_key: process.env.API_KEY,
@@ -11,37 +11,34 @@ const twit = new Twit({
 
 const mediaArtsSearch = {
   q: "#plasma",
-  count: 100,
+  count: 90,
   result_type: "recent",
 };
 
 module.exports = async function (context, req) {
-  await twit
-    .get("search/tweets", mediaArtsSearch, (error, data) => {
-      if (!error) {
-        data.statuses.forEach((status) => {
-          let retweetId = status.id_str;
-          twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
-            if (response) {
-              console.log(
-                "Success! Check your bot, it should have retweeted something."
-              );
-            }
-            if (error) {
-              console.log("There was an error with Twitter:", error);
-            }
-          });
+  twit.get("search/tweets", mediaArtsSearch, (error, data) => {
+    if (!error) {
+      data.statuses.forEach((status) => {
+        let retweetId = status.id_str;
+        twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
+          if (response) {
+            console.log(
+              "Success! Check your bot, it should have retweeted something."
+            );
+          }
+          if (error) {
+            console.log("There was an error with Twitter:", error);
+          }
         });
-      } else {
-        console.log("There was an error with your hashtag search:", error);
-        context.res = {
-          body: error.message,
-        };
-      }
-    })
-    .then(() => {
+      });
+    } else {
+      console.log("There was an error with your hashtag search:", error);
       context.res = {
-        body: "done",
+        body: error.message,
       };
-    });
+    }
+  });
+  context.res = {
+    body: "done",
+  };
 };
