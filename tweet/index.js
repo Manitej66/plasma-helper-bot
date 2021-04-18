61856448b573ace3e35ac3aa6fb44e8f9ed844e2
maniteja <pratamaniteja66@gmail.com>
2021-04-18 18:58:17 +0530

    fixed retweet bug

--- tweet/index.js
+++ tweet/index.js
@@ -10,37 +10,33 @@ const twit = new Twit({
 });
 
 const mediaArtsSearch = {
-  q: "plasma require",
+  q: "#plasma",
   count: 100,
   result_type: "recent",
 };
 
 module.exports = async function (context, req) {
-  let res;
-  twit.get("search/tweets", mediaArtsSearch, (error, data) => {
+  await twit.get("search/tweets", mediaArtsSearch, (error, data) => {
     if (!error) {
       data.statuses.forEach((status) => {
         let retweetId = status.id_str;
         twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
           if (response) {
-            res = response;
             console.log(
               "Success! Check your bot, it should have retweeted something."
             );
           }
           if (error) {
-            res = error;
             console.log("There was an error with Twitter:", error);
           }
         });
       });
     } else {
-      res = error;
       console.log("There was an error with your hashtag search:", error);
     }
   });
 
   context.res = {
-    body: res,
+    body: "doing",
   };
 };
