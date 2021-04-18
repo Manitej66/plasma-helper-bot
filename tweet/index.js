c956c33111738932ccd7cd3c1ea1217fe77760e0
maniteja <pratamaniteja66@gmail.com>
2021-04-18 19:46:20 +0530

    increased timeout

--- tweet/index.js
+++ tweet/index.js
@@ -11,32 +11,37 @@ const twit = new Twit({
 
 const mediaArtsSearch = {
   q: "#plasma",
-  count: 100,
+  count: 10,
   result_type: "recent",
 };
 
 module.exports = async function (context, req) {
-  await twit.get("search/tweets", mediaArtsSearch, (error, data) => {
-    if (!error) {
-      data.statuses.forEach((status) => {
-        let retweetId = status.id_str;
-        twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
-          if (response) {
-            console.log(
-              "Success! Check your bot, it should have retweeted something."
-            );
-          }
-          if (error) {
-            console.log("There was an error with Twitter:", error);
-          }
+  await twit
+    .get("search/tweets", mediaArtsSearch, (error, data) => {
+      if (!error) {
+        data.statuses.forEach((status) => {
+          let retweetId = status.id_str;
+          twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
+            if (response) {
+              console.log(
+                "Success! Check your bot, it should have retweeted something."
+              );
+            }
+            if (error) {
+              console.log("There was an error with Twitter:", error);
+            }
+          });
         });
-      });
-    } else {
-      console.log("There was an error with your hashtag search:", error);
-    }
-  });
-
-  context.res = {
-    body: "doing",
-  };
+      } else {
+        console.log("There was an error with your hashtag search:", error);
+        context.res = {
+          body: error.message,
+        };
+      }
+    })
+    .then(() => {
+      context.res = {
+        body: "done",
+      };
+    });
 };
