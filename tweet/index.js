a47f25103ca5b84e0e387415a72728bade0451d3
maniteja <pratamaniteja66@gmail.com>
2021-04-18 18:29:51 +0530

    update

--- tweet/index.js
+++ tweet/index.js
@@ -1,6 +1,6 @@
 const Twit = require("twit");
 
-require("dotenv").config("../.env");
+// require("dotenv").config("../.env");
 
 const twit = new Twit({
   consumer_key: process.env.API_KEY,
@@ -16,27 +16,31 @@ const mediaArtsSearch = {
 };
 
 module.exports = async function (context, req) {
+  let res;
   twit.get("search/tweets", mediaArtsSearch, (error, data) => {
     if (!error) {
       data.statuses.forEach((status) => {
         let retweetId = status.id_str;
         twit.post("statuses/retweet/" + retweetId, {}, (error, response) => {
           if (response) {
+            res = response;
             console.log(
               "Success! Check your bot, it should have retweeted something."
             );
           }
           if (error) {
+            res = error;
             console.log("There was an error with Twitter:", error);
           }
         });
       });
     } else {
+      res = error;
       console.log("There was an error with your hashtag search:", error);
     }
   });
 
   context.res = {
-    body: "doing",
+    body: res,
   };
 };
